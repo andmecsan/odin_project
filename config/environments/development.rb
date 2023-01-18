a20f4a03ae75f9b89695962d661de79c9d0db63c
Andrea Meca Sanchez <andrea.meca@factorial.co>
2023-01-18 09:29:11 +0000

    first migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.hosts << "3000-andmecsan-odinproject-spscdl3ja4g.ws-eu82.gitpod.io"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
